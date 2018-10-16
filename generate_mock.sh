ab396b2166550661d7d728e4e2d800726556150a
Daisuke TAKEUCHI <takenoco82@gmail.com>
2018-10-17 00:30:38 +0900

    [Fix] util.pyとmodels配下のみ出力するように修正

--- generate_mock.sh
+++ generate_mock.sh
@@ -1,7 +1,14 @@
 rm -rf $PWD/app
 
 docker run --rm \
-    -v $PWD/app:/tmp/out -v $PWD/templates:/tmp/templates \
+    -v $PWD/out:/tmp/out -v $PWD/templates:/tmp/templates \
     --net using-swagger-codegen_my_networks \
     swaggerapi/swagger-codegen-cli \
     generate -l python-flask -i http://swagger-ui:8080/swagger.yaml -o /tmp/out -t /tmp/templates
+
+mkdir -p $PWD/app/swagger_server
+cp $PWD/out/swagger_server/__init__.py $PWD/app/swagger_server/
+cp $PWD/out/swagger_server/util.py $PWD/app/swagger_server/
+cp -R $PWD/out/swagger_server/models $PWD/app/swagger_server/
+
+rm -rf $PWD/out
