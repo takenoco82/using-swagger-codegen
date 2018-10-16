4d6181b759a3cdd78e978a9dc92874c0258fb4d9
Daisuke TAKEUCHI <takenoco82@gmail.com>
2018-10-17 00:42:23 +0900

    [Fix] modelsのパッケージ名を指定するように修正

--- generate_mock.sh
+++ generate_mock.sh
@@ -1,14 +1,17 @@
+# modelsのパッケージ名
+PACKAGE_MODELS=swagger_codegen
+
 rm -rf $PWD/app
 
 docker run --rm \
     -v $PWD/out:/tmp/out -v $PWD/templates:/tmp/templates \
     --net using-swagger-codegen_my_networks \
     swaggerapi/swagger-codegen-cli \
-    generate -l python-flask -i http://swagger-ui:8080/swagger.yaml -o /tmp/out -t /tmp/templates
+    generate -l python-flask -i http://swagger-ui:8080/swagger.yaml -o /tmp/out -t /tmp/templates -DpackageName=$PACKAGE_MODELS
 
-mkdir -p $PWD/app/swagger_server
-cp $PWD/out/swagger_server/__init__.py $PWD/app/swagger_server/
-cp $PWD/out/swagger_server/util.py $PWD/app/swagger_server/
-cp -R $PWD/out/swagger_server/models $PWD/app/swagger_server/
+mkdir -p $PWD/app/$PACKAGE_MODELS
+cp $PWD/out/$PACKAGE_MODELS/__init__.py $PWD/app/$PACKAGE_MODELS/
+cp $PWD/out/$PACKAGE_MODELS/util.py $PWD/app/$PACKAGE_MODELS/
+cp -R $PWD/out/$PACKAGE_MODELS/models $PWD/app/$PACKAGE_MODELS/
 
 rm -rf $PWD/out
