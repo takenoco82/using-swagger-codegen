bf223796c492f47fc1be9e89cac134dc1c4a8e61
Daisuke TAKEUCHI <takenoco82@gmail.com>
2018-10-24 21:52:40 +0900

    [Add] swagger-codegenで生成したソースを追加

---
+++ app/swagger_codegen/models/__init__.py
@@ -0,0 +1,16 @@
+# coding: utf-8
+
+# flake8: noqa
+from __future__ import absolute_import
+# import models into model package
+from swagger_codegen.models.child import Child
+from swagger_codegen.models.contact import Contact
+from swagger_codegen.models.employee import Employee
+from swagger_codegen.models.error import Error
+from swagger_codegen.models.errors import Errors
+from swagger_codegen.models.grandchild import Grandchild
+from swagger_codegen.models.parent import Parent
+from swagger_codegen.models.sort import Sort
+from swagger_codegen.models.user import User
+from swagger_codegen.models.users import Users
+from swagger_codegen.models.write_user import WriteUser
---
+++ app/swagger_codegen/models/base_model_.py
@@ -0,0 +1,96 @@
+import pprint
+
+import re
+import six
+import typing
+from datetime import datetime
+
+from swagger_codegen import util
+
+T = typing.TypeVar('T')
+
+
+class Model(object):
+    # swaggerTypes: The key is attribute name and the
+    # value is attribute type.
+    swagger_types = {}
+
+    # attributeMap: The key is attribute name and the
+    # value is json key in definition.
+    attribute_map = {}
+
+    @classmethod
+    def from_dict(cls: typing.Type[T], dikt) -> T:
+        """Returns the dict as a model"""
+        return util.deserialize_model(dikt, cls)
+
+    @classmethod
+    def is_not_int(cls, value) -> bool:
+        return type(value) != int
+
+    @classmethod
+    def is_not_float(cls, value) -> bool:
+        return type(value) != float
+
+    @classmethod
+    def is_not_bool(cls, value) -> bool:
+        return type(value) != bool
+
+    @classmethod
+    def is_not_datetime(cls, value) -> bool:
+        return type(value) != datetime
+
+    @classmethod
+    def is_not_digit(cls, value) -> bool:
+        return not re.match('^\d+$', value)
+
+    @classmethod
+    def is_not_in_code(cls, code_type, value) -> bool:
+        # FIXME: code_typeに対応するリストを取得する
+        return value not in [1, 2]
+
+    def to_dict(self):
+        """Returns the model properties as a dict
+
+        :rtype: dict
+        """
+        result = {}
+
+        for attr, _ in six.iteritems(self.swagger_types):
+            value = getattr(self, attr)
+            if isinstance(value, list):
+                result[attr] = list(map(
+                    lambda x: x.to_dict() if hasattr(x, "to_dict") else x,
+                    value
+                ))
+            elif hasattr(value, "to_dict"):
+                result[attr] = value.to_dict()
+            elif isinstance(value, dict):
+                result[attr] = dict(map(
+                    lambda item: (item[0], item[1].to_dict())
+                    if hasattr(item[1], "to_dict") else item,
+                    value.items()
+                ))
+            else:
+                result[attr] = value
+
+        return result
+
+    def to_str(self):
+        """Returns the string representation of the model
+
+        :rtype: str
+        """
+        return pprint.pformat(self.to_dict())
+
+    def __repr__(self):
+        """For `print` and `pprint`"""
+        return self.to_str()
+
+    def __eq__(self, other):
+        """Returns true if both objects are equal"""
+        return self.__dict__ == other.__dict__
+
+    def __ne__(self, other):
+        """Returns true if both objects are not equal"""
+        return not self == other
