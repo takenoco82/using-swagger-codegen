1839b0c305a2c8d17569c455c8d1727dcff5f084
Daisuke TAKEUCHI <takenoco82@gmail.com>
2018-10-24 23:27:51 +0900

    [Fix] リファクタリング

--- generate_mock.sh
+++ generate_mock.sh
@@ -1,17 +1,33 @@
-# modelsのパッケージ名
-PACKAGE_MODELS=swagger_codegen
+WORKSPACE_FOLDER=`pwd`
+# swagger-codegenで生成する際のパッケージ名
+PACKAGE_CODEGEN=swagger_codegen
+# swagger-codegenでいったん出力するディレクトリ
+TMP_DIR=${WORKSPACE_FOLDER}/tmp
+# swagger-codegenで使用するテンプレートを格納するでディレクトリ
+TEMPLATES_DIR=${WORKSPACE_FOLDER}/templates
+# 最終的にswagger-codegenから出力したファイルを格納するディレクトリ
+CODEGEN_DIR=${WORKSPACE_FOLDER}/src/${PACKAGE_CODEGEN}/
 
-rm -rf $PWD/src/$PACKAGE_MODELS
+
+# swagger-codegenから出力したファイルをディレクトリごと削除
+rm -rf $CODEGEN_DIR
 
 docker run --rm \
-    -v $PWD/out:/tmp/out -v $PWD/templates:/tmp/templates \
+    -v $TMP_DIR:/tmp/out
+    -v $TEMPLATES_DIR:/tmp/templates \
     --net using-swagger-codegen_my_networks \
-    swaggerapi/swagger-codegen-cli \
-    generate -l python-flask -i http://swagger-ui:8080/swagger.yaml -o /tmp/out -t /tmp/templates -DpackageName=$PACKAGE_MODELS
+    swaggerapi/swagger-codegen-cli generate \
+        -l python-flask
+        -i http://swagger-ui:8080/swagger.yaml
+        -o /tmp/out
+        -t /tmp/templates
+        -DpackageName=$PACKAGE_CODEGEN
 
-mkdir -p $PWD/src/$PACKAGE_MODELS
-cp $PWD/out/$PACKAGE_MODELS/__init__.py $PWD/src/$PACKAGE_MODELS/
-cp $PWD/out/$PACKAGE_MODELS/util.py $PWD/src/$PACKAGE_MODELS/
-cp -R $PWD/out/$PACKAGE_MODELS/models $PWD/src/$PACKAGE_MODELS/
+# 出力したファイルのうち、必要なファイルをコピー
+mkdir -p $CODEGEN_DIR
+cp ${TMP_DIR}/$PACKAGE_CODEGEN/__init__.py $CODEGEN_DIR
+cp ${TMP_DIR}/$PACKAGE_CODEGEN/util.py $CODEGEN_DIR
+cp -R ${TMP_DIR}/$PACKAGE_CODEGEN/models $CODEGEN_DIR
 
-rm -rf $PWD/out
+# 一時ディレクトリを削除
+rm -rf $TMP_DIR
