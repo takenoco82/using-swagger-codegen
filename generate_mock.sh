afc3cdb3f7c08c7dec4b43ab5191d93eebb21559
Daisuke TAKEUCHI <takenoco82@gmail.com>
2018-10-25 22:48:22 +0900

    [Fix] 動かなくなっていのを修正

--- generate_mock.sh
+++ generate_mock.sh
@@ -13,14 +13,14 @@ CODEGEN_DIR=${WORKSPACE_FOLDER}/src/${PACKAGE_CODEGEN}/
 rm -rf $CODEGEN_DIR
 
 docker run --rm \
-    -v $TMP_DIR:/tmp/out
+    -v $TMP_DIR:/tmp/out \
     -v $TEMPLATES_DIR:/tmp/templates \
     --net using-swagger-codegen_my_networks \
     swaggerapi/swagger-codegen-cli generate \
-        -l python-flask
-        -i http://swagger-ui:8080/swagger.yaml
-        -o /tmp/out
-        -t /tmp/templates
+        -l python-flask \
+        -i http://swagger-ui:8080/swagger.yaml \
+        -o /tmp/out \
+        -t /tmp/templates \
         -DpackageName=$PACKAGE_CODEGEN
 
 # 出力したファイルのうち、必要なファイルをコピー
